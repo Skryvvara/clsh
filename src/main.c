44a221ad37d0b00ac43dc1f8e9d5c4688849cda7
Dimitri Kaiser <darkyne.gaming@outlook.de>
2024-07-21 16:54:58 +0200

    feat: added first implementation of a semi-REPL

--- src/main.c
+++ src/main.c
@@ -6,20 +6,47 @@
 
 #define EXIT_FAILURE_PARAMETERS 2
 
-int main(int argc, char* argv[]) {
+char* read_line(void) {
+    char* line = NULL;
+    ssize_t bufsize = 0;
 
-    if (argc < 2) {
-        char* name = argv[0];
-        fprintf(stderr, "usage: %s [file]\n", name);
-        exit(EXIT_FAILURE_PARAMETERS);
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
     }
+    return line;
+}
 
-    char* file_name = argv[1];
+int main(int argc, char* argv[]) {
+    char* line;
+    char* file_name;
 
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    luaL_dofile(L, file_name);
+    if (argc < 2) {
+        printf("No script file provided, reading from STDIN\n");
+
+        while (1) {
+            line = read_line();
+            luaL_dostring(L, line);
+            free(line);
+        }
+
+        /*
+        char* name = argv[0];
+        fprintf(stderr, "usage: %s [file]\n", name);
+        exit(EXIT_FAILURE_PARAMETERS);
+        */
+    } else {
+        char* file_name = argv[1];
+
+        luaL_dofile(L, file_name);
+    }
 
     lua_close(L);
     exit(EXIT_SUCCESS);
